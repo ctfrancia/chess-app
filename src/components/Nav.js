1e82db097901fb0547b79d34899d2e1595ae585b
Christian Francia <cfrancia@Christians-MBP.home>
2020-04-15 19:04:31 +0200

    add home, about component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Home from './Home';
+import About from './About';
 
 function Nav() {
 	return (
@@ -23,8 +25,10 @@ function Nav() {
 
 			<Switch>
 				<Route path="/about">
+					<About />
 				</Route>
 				<Route path="/">
+					<Home />
 				</Route>
 			</Switch>
 			</div>
